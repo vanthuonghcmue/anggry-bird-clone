3bf527f790ac472dad3b725d5d8520ca57d39998
Van Thuong <vanthuonghcmue@gmail.com>
2021-07-05 22:47:12 +0700

    fix button

---
+++ Assets/Scripts/buttonController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonController : MonoBehaviour
+{
+    /*
+       public void RestartButton()
+       {
+           SceneManager.LoadScene("SampleScene");
+       }
+
+       public void ExitButton()
+       {
+
+           SceneManager.LoadScene("main menu");
+       }
+       public void menu()
+       {
+           Debug.Log("ok");
+       }
+    */
+    /*public void loadMenu()
+     {
+         SceneManager.LoadScene("Menu");
+     }
+     public void Menu()
+     {
+         Debug.Log("loading menu...");
+     }*/
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitButton()
+    {
+
+        SceneManager.LoadScene("main menu");
+    }
+    public void menu()
+    {
+        Debug.Log("ok");
+    }
+}
+
